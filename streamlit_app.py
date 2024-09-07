d0200d86c04b845b17c6331098d2558216613fe4
Jenish Twayana <57634264+Znis@users.noreply.github.com>
2024-09-07 11:48:02 +0000

    update: implements RAG chatbot for Nepal Constitution 2072

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,81 @@
 import streamlit as st
-from openai import OpenAI
-
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_pinecone import PineconeVectorStore
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Nepal Constitution 2072 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a conversational chatbot where you can ask"
+    "questions regarding the Constitution of Nepal 2072."
+)
+OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
+LANGCHAIN_API_KEY = st.secrets['LANGCHAIN_API_KEY']
+PINECONE_API_KEY = st.secrets['PINECONE_API_KEY']
+
+LANGCHAIN_TRACING_V2 = "true"
+LANGCHAIN_ENDPOINT = "https://api.langchain.plus"
+LANGCHAIN_PROJECT = "rag-assignment-prod"
+
+
+def handle_streaming_json(streaming_json):    
+    for chunk in streaming_json:
+        if 'answer' in chunk:
+            yield chunk['answer'] 
+
+# Create an OpenAI client.
+llm = ChatOpenAI(  
+    openai_api_key=OPENAI_API_KEY,  
+    model_name='gpt-3.5-turbo',  
+    temperature=0.0  
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+index_name = "rag-assignment"
+model_name = "text-embedding-ada-002"
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+embeddings = OpenAIEmbeddings(
+    model=model_name,
+    openai_api_key=OPENAI_API_KEY
+)
+vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings, pinecone_api_key=PINECONE_API_KEY, text_key='nepal-constitution-2072')
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True,
+    output_key='answer'
+)
+retriever=vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 4})
+qa = ConversationalRetrievalChain.from_llm(
+    llm,
+    retriever=retriever,
+    memory=memory,
+    chain_type="stuff", 
+    return_source_documents=True,
+    return_generated_question=True,
+)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+# Create a session state variable to store the chat messages. This ensures that the
+# messages persist across reruns.
+if len(st.session_state) <= 0:
+    st.session_state.messages = []
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+if prompt := st.chat_input("Ask a question"):
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    # Generate a response using the OpenAI API.
+    output = qa.stream({'question': prompt})
+    # Stream the response to the chat using `st.write_stream`, then store it in 
+    # session state.
+    with st.chat_message("assistant"):
+        response = st.write_stream(handle_streaming_json(output))
+    st.session_state.messages.append({"role": "assistant", "content": response})
